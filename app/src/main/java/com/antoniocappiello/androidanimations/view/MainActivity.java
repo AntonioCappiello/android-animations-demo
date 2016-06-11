95676bf7d7e5be171224513a97b09478234b47d5
Antonio Cappiello <antoniocappiello1@gmail.com>
2016-06-11 18:05:23 +0200

    skip intro screen

--- app/src/main/java/com/antoniocappiello/androidanimations/view/MainActivity.java
+++ app/src/main/java/com/antoniocappiello/androidanimations/view/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SingleViewAnimationsActivity.class));
             }
         });
+
+        goToSingleViewAnimations(null);
     }
 
     public void goToSingleViewAnimations(View view) {
